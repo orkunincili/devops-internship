10a86439fcf14d132ad4454ef6a7e5f363df3e3b
orkunincili <orkunincili0@gmail.com>
2021-04-13 20:25:12 +0300

    DevOps internship 2021

--- README.md
+++ README.md
@@ -137,4 +137,11 @@ First of all you should **requirements.txt**:
 After that you must run create_confing.py for create confing file.It will want
 you your Jira and Bitbucket usernames and passwords.
 
-Finally you can run devops-internship.py
+Finally you can run ***devops-internship.py***
+
+
+I used two Python library that allows me to use the API for [Jira]((https://jira.readthedocs.io/en/latest/)) and [Bitbucket](https://stashy.readthedocs.io/en/latest/).
+I connect to API, get projects from Jira.
+If the project in Jira is not in Bitbucket, script add it.
+If there is error, a warning messages show up that say you should look log file
+Errors are written to the log file.
--- devops-internship.py
+++ devops-internship.py
@@ -2,12 +2,13 @@ from jira import JIRA
 import stashy
 from classes import *
 import sys
+import logging
 
 
 
 jandb = JiraAndBitbucket('config.ini')
-
-
+logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+err = False
 
 """Connect to API's"""
 jira_api = JIRA(server=jandb.jira_url(), auth=jandb.auth_jira())
@@ -16,11 +17,24 @@ bitbucket_api = stashy.connect(jandb.bitbucket_url(),
                                jandb.bitbucket_password())
 
 """Listing projects in Jira and creating projects in Bitbucket"""
+print("#### Projects in Jira ####")
 for project in jira_api.projects():
+
     print("Project name:", project)
-    bitbucket_api.projects.create(project.name, project.name)
-    
+    try:
+        new_project = bitbucket_api.projects.create(project.name, project.name)
+        print(f"Projet {new_project['name']} was created successfully in Bıtbucket")
 
+    except stashy.errors.GenericException as error:
+        err = True
+        for e in error.data['errors']:
 
-"""Listing projects in Bitbucket"""
-print(bitbucket_api.projects.list())
+            logging.error(f"Error: {e['message']} for {project}")
+
+print("\n")
+if err:
+    """Warning message stating that if there is an error, it has written it to the log file."""
+    print("WARNING - Found error(s), you may want to check 'app.log'.")
+
+#"""Listing projects in Bitbucket"""
+#print(bitbucket_api.projects.list())
